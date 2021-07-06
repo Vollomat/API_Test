8c29a71f37136ded883fd155f795e2c442082ddf
Vollomat <73344372+Vollomat@users.noreply.github.com>
2021-07-06 09:51:52 +0200

    Update AusgabeObjektGET.java

--- AusgabeObjektGET.java
+++ AusgabeObjektGET.java
@@ -4,10 +4,12 @@ public class AusgabeObjektGET {
     private Workflow workflow;
     private String workflowName;
     private boolean isActive;
+    private String href;
 
     public AusgabeObjektGET(Workflow workflow) {
         this.workflowName = workflow.getWorkflowname(); //eindeutige ID
         this.isActive = workflow.isActive();
+        this.href = "http://localhost:8080/ae/api/v1/executions";
     }
 
     public String jsonErzeugen() {
