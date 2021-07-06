8ef3e64d472693371827d62a4fa254faf13efcb1
Vollomat <73344372+Vollomat@users.noreply.github.com>
2021-07-06 09:52:19 +0200

    Update AusgabeObjektPOST.java

--- AusgabeObjektPOST.java
+++ AusgabeObjektPOST.java
@@ -6,12 +6,14 @@ public class AusgabeObjektPOST {
     private Workflow workflow;
     private String workflowName;
     private String details;
+    private String href;
 
     public AusgabeObjektPOST(Workflow workflow) {
         if (workflow != null) {
             this.runid = workflow.getRunid();
             this.workflowName = workflow.getWorkflowname();
             this.details = "Der Workflow wurde erfolgreich angestoßen!";
+            this.href = "http://localhost:8080/ae/api/v1/executions";
         } else {
             this.details = "Der gewünschte Workflow existiert nicht!";
         }
